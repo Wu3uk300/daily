10fd0cd65eeedca6f8829d8e249e957ecc2d93d1
Maksym Kondratov <geset300@gmail.com>
2024-11-13 14:36:55 +0100

    updatedArchiveMapper

--- src/components/ArchiveMapper.tsx
+++ src/components/ArchiveMapper.tsx
@@ -51,7 +51,7 @@ const ArchiveMapper: FC<ArchiveMapperInterface> = ({ advicesM, advicesP }) => {
               </>
             ) : (
               <>
-                {advicesM.map((obj) => (
+                {advicesM.map((obj: MyObject) => (
                   <div key={obj.id} className={styles.archiveBlock}>
                     <div>{obj.advice}</div>
                   </div>
@@ -69,7 +69,7 @@ const ArchiveMapper: FC<ArchiveMapperInterface> = ({ advicesM, advicesP }) => {
               </>
             ) : (
               <>
-                {advicesP.map((obj) => (
+                {advicesP.map((obj: MyObject) => (
                   <div key={obj.id} className={styles.archiveBlock}>
                     <div>{obj.advice}</div>
                   </div>
