91b233e9c8406275772426db4f7b8f73723bb6b0
Maksym Kondratov <geset300@gmail.com>
2024-11-13 15:03:07 +0100

    changedglobals

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  let prisma: PrismaClient | undefined;
 }
 
 // Этот файл необходим для расширения глобального пространства имён.
