fbb613e2296082107f54473585a99fed06bc36a1
Maksym Kondratov <geset300@gmail.com>
2024-11-27 21:14:22 +0100

    check?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
+export const revalidate = 0; // Отключаем кэширование в Next.js
 import styles from "@/styles/homePage.module.css";
 import MainHeader from "../components/MainHeader";
-import { PrismaClient } from "@prisma/client";
-export const revalidate = 0;
-export default async function Home() {
-  const prisma = new PrismaClient();
+import prisma from "@/prisma";
 
+export default async function Home() {
   const quotesCount = await prisma.quotes.count();
+
   const randomId = Math.floor(Math.random() * quotesCount) + 1;
 
   const data = await prisma.quotes.findUnique({
-    where: {
-      id: randomId,
-    },
+    where: { id: randomId },
   });
 
   return (
